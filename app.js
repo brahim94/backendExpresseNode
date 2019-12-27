8516ffde09de13faa80c959c9fcb159d41114a91
HimDev <brahim-ayad@hotmail.com>
2019-12-27 19:48:04 +0100

    mongodb conection

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
-const app = express();
+mongoose.connect('mongodb+srv://brahimmongo:25Mongo@cluster0-xhtjj.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
