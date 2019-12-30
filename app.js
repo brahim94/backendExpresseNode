a3eb6394c039bc2eeb96674057410944a07f2cd8
HimDev <brahim-ayad@hotmail.com>
2019-12-30 16:04:38 +0100

    fixing CROS issue

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ mongoose.connect('mongodb+srv://brahimmongo:25Mongo@cluster0-xhtjj.mongodb.net/t
 
   const app = express();
 
-app.use((req, res, next) => {
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Acces-Control-Allow-Methodes', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
 app.use(bodyParser.json());
 
