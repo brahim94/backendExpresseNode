5a9f025e98ebde9491ab582ef56a4198a92e2bcc
HimDev <brahim-ayad@hotmail.com>
2019-12-30 16:45:04 +0100

    deleting one thing

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.put('/api/stuff/:id', (req, res, next) => {
     
 });
 
+app.delete('/api/stuff/:id', (req, res, next) => 
+{
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+
+});
+
 app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
