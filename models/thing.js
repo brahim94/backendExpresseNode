8b5d752399477b3308dcb41ad472c215a71fd704
HimDev <brahim-ayad@hotmail.com>
2019-12-30 16:25:08 +0100

    fixing price issue

--- models/thing.js
+++ models/thing.js
@@ -4,8 +4,10 @@ const thingSchema = mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     imageUrl: { type: String, required: true },
-    userId: { type: String, required: true}
-});
+    userId: { type: String, required: true },
+    price: { type: Number, required: true },
+  });
+  
 
 module.exports = mongoose.model('Thing', thingSchema);
 
